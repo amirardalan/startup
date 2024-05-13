11b34fe59697b5b86b5308a5d32fea6743a44099
Amir Ardalan <hi@amirardalan.com>
2024-05-12 23:18:44 -0700

    Update README.md

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-lg lg:flex">
-        Hello World
+        Startup
       </div>
     </main>
   );
