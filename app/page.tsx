6d31f8acc951b075c29f5856ddcf32cdabc00a7f
Amir Ardalan <hi@amirardalan.com>
2024-05-17 23:06:36 -0700

    Install prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex justify-center align-center h-screen">
-      <div className="font-mono text-gray-500 text-center text-3xl m-auto">
-        Startup()
+    <main className="flex flex-column justify-center align-center h-screen">
+      <div className="m-auto text-center">
+        <div className="font-mono text-gray-500 text-center text-3xl">
+          Startup()
+        </div>
+        <div className="text-black">subtitle</div>
       </div>
     </main>
   );
