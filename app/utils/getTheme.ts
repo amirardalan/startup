011ce86544b96772d971067b6af3307fc62ca2b3
Amir <hi@amirardalan.com>
2024-06-06 13:17:32 -0700

    Fix system theme check logic

--- app/utils/getTheme.ts
+++ app/utils/getTheme.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 export function getTheme() {
   let theme = cookies().get('x-theme')?.value;
 
-  if (theme === 'system') {
+  if (!theme || theme === 'system') {
     theme =
       cookies().get('x-system-theme')?.value === 'dark' ? 'dark' : 'light';
   } else if (theme !== 'dark') {
