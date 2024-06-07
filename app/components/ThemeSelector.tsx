90cf13fc9567508208bd9c60a926cbb29eb392e0
Amir <hi@amirardalan.com>
2024-06-07 13:35:51 -0700

    Improve theme toggle logic

--- app/components/ThemeSelector.tsx
+++ app/components/ThemeSelector.tsx
@@ -3,47 +3,71 @@ import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
 import { Theme } from '@/types/theme';
+import clsx from 'clsx';
 
 type ThemeProps = {
   theme: Theme;
 };
 
 export default function ThemeSelector({ theme }: ThemeProps) {
-  const [currentTheme, setCurrentTheme] = useState(theme);
+  const [currentTheme, setCurrentTheme] = useState<Theme>(theme);
 
   useEffect(() => {
-    if (window.matchMedia) {
+    const themeFromCookie = Cookies.get('x-theme') as Theme;
+
+    if (themeFromCookie) {
+      setCurrentTheme(themeFromCookie);
+    } else if (window.matchMedia) {
       const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      Cookies.set('x-system-theme', isDark ? 'dark' : 'light');
+      Cookies.set('x-theme', isDark ? 'dark' : 'light');
       setCurrentTheme(isDark ? 'dark' : 'light');
     }
   }, []);
 
+  console.log(currentTheme);
+
   const router = useRouter();
+
+  const handleManualTheme = () => {
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    Cookies.set('x-theme', newTheme);
+    setCurrentTheme(newTheme);
+    router.refresh();
+  };
+
+  const handleSystemTheme = () => {
+    Cookies.set('x-theme', 'system');
+    setCurrentTheme('system');
+    window.location.reload();
+  };
 
   return (
     <div>
       <button
-        onClick={() => {
-          const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-          Cookies.set('x-theme', newTheme);
-          setCurrentTheme(newTheme);
-          router.refresh();
-        }}
+        onClick={() => handleManualTheme()}
         aria-label="switch theme"
-        className="rounded bg-primary px-2 py-1 text-xs font-semibold text-dark shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
+        className={clsx(
+          'rounded px-2 py-1 text-xs font-semibold text-dark shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary',
+          {
+            'bg-primary': currentTheme === 'light' || 'dark',
+            'bg-secondary': currentTheme === 'system',
+          }
+        )}
       >
-        Switch to {currentTheme === 'dark' ? 'light' : 'dark'} mode
+        {currentTheme === 'dark' ? '☀️' : '🌑'}
       </button>
       <button
-        onClick={() => {
-          Cookies.set('x-theme', 'system');
-          window.location.reload();
-        }}
+        onClick={() => handleSystemTheme()}
         aria-label="set system theme"
-        className="ml-2 rounded bg-secondary px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
+        className={clsx(
+          'ml-2 rounded px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary',
+          {
+            'bg-primary': currentTheme === 'system',
+            'bg-secondary': currentTheme === 'light' || 'dark',
+          }
+        )}
       >
-        System Mode
+        🖥️
       </button>
     </div>
   );
