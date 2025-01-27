224f2973a05da964b4512d28e33f1fb50372caf5
amirardalan <hi@amir.sh>
2025-01-27 10:27:58 -0800

    Convert signout button to client side

--- app/components/auth/signout-button.tsx
+++ app/components/auth/signout-button.tsx
@@ -1,18 +1,22 @@
-import { signOut } from '@/auth';
+'use client';
+
+import { signOut } from 'next-auth/react';
 
 export default function SignOutButton() {
   return (
     <form
-      action={async () => {
-        'use server';
-        await signOut();
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await signOut({
+          callbackUrl: '/',
+        });
       }}
     >
       <button
         type="submit"
         className="text-xxs m-0 min-w-24 rounded-lg bg-dark px-2 py-1.5 font-mono uppercase text-light dark:bg-light dark:text-dark"
       >
-        Sign out
+        Sign Out
       </button>
     </form>
   );
