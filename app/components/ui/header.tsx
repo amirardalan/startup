95be816709d339aeb520de1f28fd49cd337e4ec2
amirardalan <hi@amir.sh>
2025-01-31 23:56:12 -0800

    Update account link

--- app/components/ui/header.tsx
+++ app/components/ui/header.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@/auth';
 import ThemeMenu from '@/components/theme/theme-menu';
 import AuthMenu from '@/app/components/auth/menu';
+import Link from 'next/link';
 
 export default async function Header() {
   const session = await auth();
@@ -8,9 +9,14 @@ export default async function Header() {
   return (
     <div className="flex flex-row items-center justify-between">
       <div className="font-mono text-xxs uppercase text-dark dark:text-light">
-        {session
-          ? `Welcome, ${session?.user?.name?.split(' ')[0]}`
-          : 'Welcome, Guest'}
+        {session ? (
+          <>
+            Welcome,&nbsp;
+            <Link href="/account">{session?.user?.name?.split(' ')[0]}</Link>
+          </>
+        ) : (
+          'Welcome, Guest'
+        )}
       </div>
       <div className="flex">
         <div className="mr-6 mt-1 flex items-center align-middle">
