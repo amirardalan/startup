34695e0c68b1645a926b1f59ed29435f8a7e88c2
amirardalan <hi@amir.sh>
2025-02-03 17:50:38 -0800

    Update footer

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -4,16 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-row text-left text-sm text-gray-400">
       <div className="mr-6 text-gray-400">
-        &copy; {new Date().getFullYear()}
-        <a
-          href="https://github.com/amirardalan"
-          target="blank"
-          rel="noopener noreferrer"
-          title="@amirardalan • GitHub"
-          className="ml-2 hover:underline"
-        >
-          Amir Ardalan
-        </a>
+        &copy; {new Date().getFullYear()} Startup()
       </div>
       <span className="text-gray-300 dark:text-gray-600">|</span>
       <div className="ml-6">
