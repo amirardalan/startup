2d7e1cc6e4c07042665cde18eaf93a74bb3e1a97
amirardalan <hi@amir.sh>
2025-02-09 11:03:12 -0800

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <h2 className="mb-6 border-b-2 border-solid border-gray-300 pb-4 text-xxl text-dark dark:border-gray-600 dark:text-light">
           About
         </h2>
-        <p className="text-dark dark:text-light">Account content</p>
+        <p className="text-dark dark:text-light">About content</p>
       </div>
     </main>
   );
