0ce994de8f53fbc269a7ab035841ad04ce3cb666
Amir Ardalan <hi@amir.sh>
2025-07-13 13:34:54 -0700

    Fix header link

--- src/components/ui/HeaderExternalLinks.tsx
+++ src/components/ui/HeaderExternalLinks.tsx
@@ -11,7 +11,7 @@ export default function HeaderExternalLinks() {
 
   const handleClick = () => {
     window.open(
-      'https://github.com/amirardalan/startup',
+      'https://github.com/amirardalan/amirardalan.com',
       '_blank',
       'noopener,noreferrer'
     );
